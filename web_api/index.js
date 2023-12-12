2b5799b4ce889f53e779a0db37038f5a18e80f5e
Karl MARQUES <karl@kmarques.dev>
2023-12-12 16:09:00 +0100

    storage API

--- web_api/index.js
+++ web_api/index.js
@@ -11,27 +11,60 @@ function changeIntoInput(event) {
   const text = textNode.textContent;
   const input = document.createElement("input");
   input.setAttribute("value", text);
-  input.addEventListener("", changeIntoText);
 
-  td.replaceChild(input, textNode);
+  input.addEventListener("blur", changeIntoText);
   td.removeEventListener("click", changeIntoInput);
+  td.replaceChild(input, textNode);
   input.focus();
 }
 
 function changeIntoText(event) {
   const input = event.currentTarget;
+  const text = input.value;
+  const textNode = document.createTextNode(text);
+  const td = input.parentNode;
+
+  data[td.dataset.coordinate] = text;
+  const dataString = JSON.stringify(data);
+  localStorage.setItem("content", dataString);
+
+  input.removeEventListener("blur", changeIntoText);
+  td.addEventListener("click", changeIntoInput);
+  td.replaceChild(textNode, input);
 }
 
-for (let i = 0; i < 5; i++) {
+const dataString = localStorage.getItem("content");
+const data = JSON.parse(dataString || "{}");
+
+for (let i = 0; i < 15; i++) {
   const tr = document.createElement("tr");
   tbody.appendChild(tr);
-  for (let j = 0; j < 5; j++) {
+  for (let j = 0; j < 15; j++) {
     const td = document.createElement("td");
     tr.appendChild(td);
-    const text = document.createTextNode(defaultText);
+    td.dataset.coordinate = `${i}x${j}`;
+    const text = document.createTextNode(
+      data[td.dataset.coordinate] ?? defaultText
+    );
     td.addEventListener("click", changeIntoInput);
     td.appendChild(text);
   }
 }
 
 root.appendChild(table);
+
+//window.addEventListener("mousemove", function (event) {
+//  console.log(event.clientX, event.clientY);
+//});
+//
+//document.addEventListener("visibilitychange", () => {
+//  if (document.visibilityState === "visible") {
+//    console.log("foreground");
+//  } else {
+//    console.log("background");
+//  }
+//});
+//
+//document.addEventListener("mouseleave", function (e) {
+//  console.log("screen changed");
+//});
